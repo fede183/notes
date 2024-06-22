2736791b71cf8e5a1b525b745fd94b104799bd36
fede183 <fede.183@hotmail.com>
2024-06-22 16:34:38 -0300

    groff in diving

---
+++ diving/README.md
@@ -0,0 +1,1 @@
+groff -ms Lugares.ms -T pdf > Lugares.pdf && zathura Lugares.pdf
---
+++ diving/build.sh
@@ -0,0 +1,7 @@
+!#/bin/sh
+
+files=$(ls | grep ".ms" | sed "s/\..\+$//g")
+
+for file in $files; do
+	groff -ms $file.ms -T pdf > $file.pdf
+done
